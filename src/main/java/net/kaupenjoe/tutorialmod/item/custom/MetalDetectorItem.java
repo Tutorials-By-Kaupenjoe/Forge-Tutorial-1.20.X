1037fd15c0031f373345169c14ce57a1bee18559
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-07-28 16:20:12 +0200

    custom tags

--- src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.kaupenjoe.tutorialmod.item.custom;
 
+import net.kaupenjoe.tutorialmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -63,6 +64,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
---
+++ src/main/java/net/kaupenjoe/tutorialmod/util/ModTags.java
@@ -0,0 +1,27 @@
+package net.kaupenjoe.tutorialmod.util;
+
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> METAL_DETECTOR_VALUABLES = tag("metal_detector_valuables");
+
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
+        }
+    }
+}
