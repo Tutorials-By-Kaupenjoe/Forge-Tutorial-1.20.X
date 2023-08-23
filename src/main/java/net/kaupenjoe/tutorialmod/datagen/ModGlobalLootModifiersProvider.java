b433db2153a0debbe2c4daf117ca45b839602567
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-08-23 16:20:06 +0200

    sus sand item

--- src/main/java/net/kaupenjoe/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/kaupenjoe/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
@@ -3,6 +3,7 @@ package net.kaupenjoe.tutorialmod.datagen;
 import net.kaupenjoe.tutorialmod.TutorialMod;
 import net.kaupenjoe.tutorialmod.item.ModItems;
 import net.kaupenjoe.tutorialmod.loot.AddItemModifier;
+import net.kaupenjoe.tutorialmod.loot.AddSusSandItemModifier;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
@@ -28,6 +29,12 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
 
         add("metal_detector_from_jungle_temples", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() }, ModItems.METAL_DETECTOR.get()));
+
+
+        add("metal_detector_from_suspicious_sand", new AddSusSandItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("archaeology/desert_pyramid")).build() }, ModItems.METAL_DETECTOR.get()));
+
+
 
 
     }
--- src/main/java/net/kaupenjoe/tutorialmod/loot/ModLootModifiers.java
+++ src/main/java/net/kaupenjoe/tutorialmod/loot/ModLootModifiers.java
@@ -14,6 +14,8 @@ public class ModLootModifiers {
 
     public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_ITEM =
             LOOT_MODIFIER_SERIALIZERS.register("add_item", AddItemModifier.CODEC);
+    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_SUS_SAND_ITEM =
+            LOOT_MODIFIER_SERIALIZERS.register("add_sus_sand_item", AddSusSandItemModifier.CODEC);
 
 
     public static void register(IEventBus eventBus) {
